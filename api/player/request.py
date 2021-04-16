966b7e9a4e8eab10f1e020f9c888e9660b0da7ea
kiobu <cdolson30@gmail.com>
2021-04-16 17:31:06 -0400

    Fix incorrect class variable identifiers.

--- api/player/request.py
+++ api/player/request.py
@@ -3,11 +3,11 @@ from api.models import Endpoint, Consts
 
 
 class PlayerRequest(Request):
-    def __init__(self, match_id: int):
+    def __init__(self, player_id: int):
         super().__init__()
-        self.match_id = match_id
+        self.player_id = player_id
 
-        self.url += Endpoint.player.value + str(self.match_id)  # Set request URL to include the match ID.
+        self.url += Endpoint.player.value + str(self.player_id)  # Set request URL to include the match ID.
         self._attach_uuid()
 
     def _attach_uuid(self):
